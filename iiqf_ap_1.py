f8e7b08e4eadf559edba201924bfa1c13fddbe4f
apaulcloud <c801ap@gmail.com>
2022-04-24 02:01:36 -0400

    print
    Signed-off-by: apaulcloud <c801ap@gmail.com>

---
+++ iiqf_ap_1.py
@@ -0,0 +1,11 @@
+# home work May 7th
+#
+# basics of intellij
+#
+# check how get plugins for putho
+#     create python project
+# share the link of recorded sesson
+# create pme pythn projct with intellij
+#     take jupyter notebook codde and write to one .py file and create and run
+
+print(5+5)
